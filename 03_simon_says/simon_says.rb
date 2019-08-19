45f4397d8e42334af4bbb8cf088d41d0968627da
TheWillGabriel <will.gabriel@gmail.com>
2019-08-18 21:11:00 -0600

    Create start of word function

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,7 +6,12 @@ def shout command
   return command.upcase
 end
 
-def repeat (command, times=2)
-  return ("#{command} " * times).strip
+def repeat ( command, times=2 )
+  return ( "#{command} " * times ).strip
+end
+
+def start_of_word ( command, how_many=1 )
+  index = how_many - 1
+  return command[0..index]
 end
 
