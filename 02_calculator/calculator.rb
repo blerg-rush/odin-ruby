2e6acad0eef05dcb1a587eb34531f1970bf095b0
TheWillGabriel <will.gabriel@gmail.com>
2019-08-18 20:12:45 -0600

    Create sum function

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,3 +6,12 @@ def subtract ( first_number, second_number )
   return first_number - second_number
 end
 
+def sum array
+  sum = 0
+
+  array.each do |number|
+    sum += number
+  end
+
+  return sum
+end
