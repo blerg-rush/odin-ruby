03744419c318b677d2cf335b236461ba86336599
TheWillGabriel <will.gabriel@gmail.com>
2019-08-26 16:36:41 -0600

    Create my_each method

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-# This Enumerable add-on extends the existing module with alternative methods
-
+# Extends the existing module with alternative methods
 module Enumerable
   def my_each
-
+    i = 0
+    while i < size
+      yield(self[i])
+      i += 1
+    end
+    self
   end
 end
