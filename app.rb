6308bd7f16d49c598333c0267d1294748d98dc96
TheWillGabriel <will.gabriel@gmail.com>
2019-08-27 14:53:34 -0600

    Configure routes

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "./caesar_cipher"
 
+enable :sessions
+
 get "/" do
-  
+  @message = session[:message]
+  erb :index
+end
+
+post "/" do
+  @string = params["string"]
+  @offset = params["offset"]
+  session[:message] = "Your encoded text: #{cipher(@string, @offset)}"
+  redirect "/"
 end
