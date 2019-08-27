3fcd06afb86c25a2ef6daaeb41e96eccac39d22d
TheWillGabriel <will.gabriel@gmail.com>
2019-08-27 15:02:16 -0600

    Change character storage from string to array

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal:true
+
 def cipher(string, offset)
   alphabet = ("a".."z").to_a
   offset = offset.to_i
-  encrypted_string = ""
+  encrypted_array = []
   string.each_char do |character|
     if character =~ /[^A-z]/
-      encrypted_string << character
+      encrypted_array << character
       next
     end
 
@@ -12,11 +14,11 @@ def cipher(string, offset)
     index = alphabet.find_index(character.downcase)
     shifted_character = alphabet[(index + offset) % 26]
 
-    encrypted_string << if is_upcase
-                          shifted_character.upcase
-                        else
-                          shifted_character
-                        end
+    encrypted_array << if is_upcase
+                         shifted_character.upcase
+                       else
+                         shifted_character
+                       end
   end
-  encrypted_string
+  encrypted_array.join("")
 end
