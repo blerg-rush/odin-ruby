8e9e396f7b40a4bbe647e45fe49bdbd030ca3e8d
TheWillGabriel <will.gabriel@gmail.com>
2019-08-27 14:35:06 -0600

    Use regex to ignore non-alphanumeric characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,21 +1,23 @@
-def cipher( string, offset )
+# frozen_string_literal: true
+
+def cipher(string, offset)
   alphabet = ("a".."z").to_a
   encrypted_string = ""
   string.each_char do |character|
-    if character == " "
-      encrypted_string << " "
+    if character =~ /\W/
+      encrypted_string << character
       next
     end
 
-    is_upcase = character == character.upcase #Will need to convert back if it is
-    index = alphabet.find_index( character.downcase )
-    shifted_character = alphabet[( index + offset ) % 26 ]
+    is_upcase = character == character.upcase
+    index = alphabet.find_index(character.downcase)
+    shifted_character = alphabet[(index + offset) % 26]
 
-    if is_upcase
-      encrypted_string << shifted_character.upcase
-    else
-      encrypted_string << shifted_character
-    end
+    encrypted_string << if is_upcase
+                          shifted_character.upcase
+                        else
+                          shifted_character
+                        end
   end
-  return encrypted_string
+  encrypted_string
 end
