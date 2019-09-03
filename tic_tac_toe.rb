2b740d867c53f681f5c5a731aafb6d30a6fad156
TheWillGabriel <will.gabriel@gmail.com>
2019-09-03 16:34:28 -0600

    Bugfixes and lots of code smell

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,17 +14,17 @@ end
 # Contains methods for creating, setting spaces, and checking state
 class Board
   def initialize
-    @state = [["", "", ""],
-              ["", "", ""],
-              ["", "", ""]].freeze
+    @state = [[" ", " ", " "],
+              [" ", " ", " "],
+              [" ", " ", " "]].freeze
   end
 
   def state
     state_string = ""
     @state.each do |row|
-      state_string += row.join + "\n"
+      state_string += "#{row.join}\n"
     end
-    state_string
+    state_string.tr(" ", "⛶")
   end
 
   def get_space(row, column)
@@ -35,24 +35,20 @@ class Board
 
   # returns false if it fails
   def set_space(space, glyph)
-    row = space[0]
-    column = space[1]
+    row = space[0].to_i
+    column = space[1].to_i
     success = false
-    unless state[row - 1][column - 1] == ""
-      begin
-        @state[row - 1][column - 1] = glyph
-        success = true
-      rescue StandardError
-        puts "#{row},#{column} is not a valid space!"
-      end
+    if @state[row - 1][column - 1] == " "
+      @state[row - 1][column - 1] = glyph
+      success = true
     end
     success
   end
 
   def full?
     full = true
-    state.each do |board_row|
-      full = false if board_row.any? { |board_space| board_space == "" }
+    @state.each do |board_row|
+      full = false if board_row.any? { |board_space| board_space == " " }
     end
     full
   end
@@ -67,7 +63,7 @@ class Board
 
   def horizontal_line?(glyph)
     line = false
-    state.each do |board_row|
+    @state.each do |board_row|
       line = true if board_row.all? { |board_space| board_space == glyph }
     end
     line
@@ -75,20 +71,20 @@ class Board
 
   def vertical_line?(glyph)
     line = false
-    state.transpose.each do |board_row|
+    @state.transpose.each do |board_row|
       line = true if board_row.all? { |board_space| board_space == glyph }
     end
     line
   end
 
   def diagonal_line?(glyph)
-    return true if state[0][0] == glyph &&
-                   state[1][1] == glyph &&
-                   state[2][2] == glyph
+    return true if @state[0][0] == glyph &&
+                   @state[1][1] == glyph &&
+                   @state[2][2] == glyph
 
-    return true if state[0][2] == glyph &&
-                   state[1][1] == glyph &&
-                   state[2][0] == glyph
+    return true if @state[0][2] == glyph &&
+                   @state[1][1] == glyph &&
+                   @state[2][0] == glyph
 
     false
   end
@@ -104,16 +100,18 @@ class Game
   end
 
   def play
-    setup if last_winner.nil?
+    setup if @last_winner.nil?
     @current_player = first
-    puts "#{@current_player} goes first!"
-    turn(@current_player) until @board.full? || @board.win?
+    puts "#{@current_player.name} goes first!"
+    turn(@current_player) until @board.full?     ||
+                                @board.win?("X") ||
+                                @board.win?("O")
     end_game
   end
 
   private
 
-  attr_writer :player_x, :player_y
+  attr_writer :player_x, :player_o
 
   def coin_toss
     rand(2) == 1 ? "X" : "O"
@@ -121,9 +119,9 @@ class Game
 
   def first
     if @last_winner.nil?
-      coin_toss == "X" ? "X" : "O"
+      coin_toss == "X" ? @player_x : @player_o
     else
-      @last_winner == "X" ? "O" : "X"
+      @last_winner == "X" ? @player_o : @player_x
     end
   end
 
@@ -139,7 +137,7 @@ class Game
 
   def setup
     create_player("X")
-    create_player("Y")
+    create_player("O")
   end
 
   def turn(player)
@@ -147,16 +145,42 @@ class Game
     success = false
     until success
       begin
-        puts "Where would #{player} like to place
-          their #{player.glyph}? (row,column)"
+        puts "Where would #{player.name} like to place their #{player.glyph}? (row,column)"
         space = gets.chomp.split(",")
+        raise "Goof!" unless space.size == 2              &&
+                             %w[1 2 3].include?(space[0]) &&
+                             %w[1 2 3].include?(space[1]) # Fix hardcoded excepton later
       rescue StandardError
         puts "Invalid input."
+        retry
       end
       success = @board.set_space(space, player.glyph)
+      puts "That space is taken!" unless success
     end
-    @current_player = @player_x if player == @player_y
-    @current_player = @player_y if player == @player_x
+    @current_player = @player_x if player == @player_o
+    @current_player = @player_o if player == @player_x
   end
 
+  def end_game
+    display_board
+    if @board.win?("X")
+      @last_winner = "X"
+      puts "#{@player_x.name} wins!"
+    elsif @board.win?("O")
+      @last_winner = "O"
+      puts "#{@player_o.name} wins!"
+    else
+      puts "It's a draw..."
+    end
+    puts "Play again? (y/n)"
+    if gets.chomp == "y"
+      game = Game.new(@player_x.name, @player_o.name, @last_winner)
+      game.play
+    else
+      puts "Goodbye!"
+    end
+  end
 end
+
+game = Game.new
+game.play
