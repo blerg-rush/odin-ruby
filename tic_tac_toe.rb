34492fcdce5245c89baa8fea9c678ac33227415e
TheWillGabriel <will.gabriel@gmail.com>
2019-09-03 11:40:27 -0600

    Create Board class with getters and setters

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,4 +11,25 @@ class Player
   end
 end
 
+# Contains methods for creating, setting squares, and checking state
+class Board
+  attr_reader :state
 
+  def initialize
+    @state = [["", "", ""],
+              ["", "", ""],
+              ["", "", ""]].freeze
+  end
+
+  def get_square(row, column)
+    state[row - 1][column - 1]
+  rescue StandardError
+    puts "#{row},#{column} is not a valid space!"
+  end
+
+  def set_square(row, column, glyph) # glyph = X or O, depending on player 
+    @state[row - 1][column - 1] = glyph
+  rescue StandardError
+    puts "#{row},#{column} is not a valid space!"
+  end
+end
