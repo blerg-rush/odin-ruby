4ce07a5236121c05eec5ac4890a28244422f4e58
TheWillGabriel <will.gabriel@gmail.com>
2019-09-04 12:00:28 -0600

    Create test for User validity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Test User", email: "real.email42@thezone.ca")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
