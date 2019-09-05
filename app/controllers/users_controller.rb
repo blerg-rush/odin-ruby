9f00e508e4f29f58530ec9b34c7ff169404b9741
TheWillGabriel <will.gabriel@gmail.com>
2019-09-05 15:48:20 -0600

    Send new user to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Do it
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
