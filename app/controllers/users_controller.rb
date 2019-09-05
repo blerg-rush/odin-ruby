733918233c609b9f41291f896e6050add8354bb9
TheWillGabriel <will.gabriel@gmail.com>
2019-09-05 11:45:53 -0600

    Add edit User form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -17,7 +18,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.notice = "Successfully edited #{@user.name}"
+      redirect_to edit_user_path
+    else
+      flash.notice = "Error: #{@user.errors.messages}"
+      render :edit
+    end
+  end
+
   def user_params
-    params.require(:post).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
