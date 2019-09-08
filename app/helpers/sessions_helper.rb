220459efefd2ed5e96fc07f65e92c99900ce5f2a
TheWillGabriel <will.gabriel@gmail.com>
2019-09-08 10:09:13 -0600

    Add helper method to get current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,11 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
+
+  # Gets user by ID in session cookie
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
