51aad7bcffd6c07cf7b2ccb5c4655fe836d7e19d
TheWillGabriel <will.gabriel@gmail.com>
2019-09-07 20:19:31 -0600

    Add notification to login if authentication fails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in and go to user's profile
+    else
+      flash.now[:danger] = "Invalid user credentials"
+      render :new
+    end
+  end
+
+  def destroy
+  end
 end
