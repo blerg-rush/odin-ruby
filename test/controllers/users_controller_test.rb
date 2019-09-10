93b2abffe6ff3498b160b1260a58ad0f8f0ba080
TheWillGabriel <will.gabriel@gmail.com>
2019-09-10 10:21:22 -0600

    Add all users view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,11 +31,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "Should redirect update when logged in as wrong user" do
+  test "should redirect update when logged in as wrong user" do
     login_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 end
