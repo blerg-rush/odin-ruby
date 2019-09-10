c46ceb4f84f7e1ef7c521b0062e7b20808611936
TheWillGabriel <will.gabriel@gmail.com>
2019-09-10 09:17:09 -0600

    Create test for unauthorized edit or update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+
   def new
     @user = User.new
   end
@@ -25,8 +27,9 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      # successfully update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
 #     flash.now[:danger] = "Invalid user credentials"
       render :edit
@@ -37,5 +40,15 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
