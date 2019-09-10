fabb1993724dc863c26760395580062de26468f7
TheWillGabriel <will.gabriel@gmail.com>
2019-09-10 10:52:31 -0600

    Update layout test for logged-in user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:example)
+  end
+
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -16,5 +20,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    login_as(@user)
+    get users_path
+    assert_select "a[href=?]", user_path(@user)
+    get user_path(@user)
+    assert_select "section[class=?]", "user_info"
+    get edit_user_path
+    assert_select "title", full_title("Edit user")
   end
 end
