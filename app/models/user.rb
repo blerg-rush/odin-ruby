f343229ad8b6e2bc4856fbe166ba876d9124dc74
TheWillGabriel <will.gabriel@gmail.com>
2019-09-09 20:18:55 -0600

    Remove session cookie on logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,17 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  # Remembers a user in database for persistent sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if token matches the digest
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
