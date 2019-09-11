9a86420b5839a19d62a2a665436b6e36c714be73
TheWillGabriel <will.gabriel@gmail.com>
2019-09-11 08:22:03 -0600

    Create account_activation method in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
+  def account_activation(user)
     @greeting = "Hi"
-
+    @user = user
     mail to: user.email, subject: "Account activation"
   end
 
