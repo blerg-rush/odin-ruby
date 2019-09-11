2f00f4229698cab3a8d08f912e4be0955e5378e3
TheWillGabriel <will.gabriel@gmail.com>
2019-09-11 12:50:18 -0600

    Add activation to signup integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
@@ -10,19 +15,36 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
       post users_path, params: { user: { name: "Agood User",
-                                         email: "good@email.com",
+                                         email: "good@example.com",
                                          password: "testpassword1",
                                          password_confirmation: "testpassword1" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation
+    login_as(user)
+    assert_not logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    # assert_template "users/show"
-    # assert logged_in?
+    assert_template "users/show"
+    assert logged_in?
     # assert_not flash.empty?
   end
 end
