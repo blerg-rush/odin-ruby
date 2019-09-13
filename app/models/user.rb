14baea1ad6a4ec1644a5b9704ef85f6e351e427a
TheWillGabriel <will.gabriel@gmail.com>
2019-09-13 09:29:04 -0600

    Remember user on account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+  before_create :remember
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def remember
+      self.remember_digest = User.digest(User.new_token)
+    end
 end
