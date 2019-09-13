3761474a1304755f88135a5d3a55cbcad09159f8
TheWillGabriel <will.gabriel@gmail.com>
2019-09-13 09:52:30 -0600

    Create login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def login(user)
+    new_token = User.new_token
+    cookies.permanent[:remember_token] = new_token
+    user.remember_digest = User.digest(new_token)
+  end
 end
