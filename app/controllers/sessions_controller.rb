bc0c16a7183422100fb7dc4a36a3fcf4bc97c183
TheWillGabriel <will.gabriel@gmail.com>
2019-09-13 09:38:22 -0600

    Add session create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      flash[:success] = "Alright, come in. Quickly!"
+      redirect_to root_url # TODO: Send to posts index
+    else
+      flash.now[:danger] = "You're not on the list. Get lost!"
+      render :new
+    end
   end
 
   def destroy
