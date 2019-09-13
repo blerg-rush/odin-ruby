0834bd4dfa4f0797d28224cd933000e40875fcf0
TheWillGabriel <will.gabriel@gmail.com>
2019-09-13 10:00:43 -0600

    Create current_user getter and setter methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  attr_writer :current_user
 
   def login(user)
     new_token = User.new_token
     cookies.permanent[:remember_token] = new_token
     user.remember_digest = User.digest(new_token)
+    @current_user = user
+  end
+
+  def current_user
+    remember_digest = User.digest(cookies.permanent[:remember_token])
+    @current_user ||= User.find_by(remember_digest: remember_digest)
   end
 end
