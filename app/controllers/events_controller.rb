def65dfda1d6e0c39d23a97615c018421fd2ddb4
TheWillGabriel <will.gabriel@gmail.com>
2019-09-16 14:14:22 -0600

    Create event form and add to user profile

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,27 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    user = current_user
+    @event = user.hosted_events.build(event_params)
+    if @event.save
+      flash[:success] = "Event created"
+      redirect_to @event
+    else
+      flash.now[:danger] = @event.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :date)
+    end
 end
