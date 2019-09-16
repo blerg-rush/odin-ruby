d0318bb079f51e4d5a7d55b157ff1724b6563578
TheWillGabriel <will.gabriel@gmail.com>
2019-09-16 10:36:28 -0600

    Add password digest to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :hosted_events, class_name: "Event", foreign_key: "host_id",
                            dependent: :destroy
   has_many :invitations, foreign_key: "attendee_id", dependent: :destroy
