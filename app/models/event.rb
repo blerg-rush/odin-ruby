047c8fa2a597de7fea501f3788d131cbad9efc88
TheWillGabriel <will.gabriel@gmail.com>
2019-09-16 12:57:56 -0600

    Add validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :invitations, dependent: :destroy
   has_many :attendees, through: :invitations
+  validates :title, presence: true
+  validates :date, presence: true
 end
