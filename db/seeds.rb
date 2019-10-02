53a85e27e2f83965358127146055e24158dccb94
TheWillGabriel <will.gabriel@gmail.com>
2019-10-02 13:59:46 -0600

    Seed db with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Airport.delete_all
+Flight.delete_all
 
 Airport.create(code: 'YEG')
 
@@ -7,4 +8,15 @@ Airport.create(code: 'YYC')
 10.times do
   code = (0..2).map{ ('A'..'Z').to_a[rand(26)] }.join
   Airport.create(code: code)
+end
+
+Airport.first.departing_flights.build(to_id: Airport.second.id).save
+
+Airport.second.departing_flights.build(to_id: Airport.first.id).save
+
+10.times do
+  airports = Airport.all
+  from = airports.shuffle.pop
+  to = airports.shuffle.pop
+  from.departing_flights.build(to_id: to.id).save
 end
