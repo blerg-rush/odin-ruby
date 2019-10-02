daecca16de568d4ca313a535ac9d105d60be5c2f
TheWillGabriel <will.gabriel@gmail.com>
2019-10-02 14:04:56 -0600

    Generate flights controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'flights/index'
+  get 'flights/show'
+  get 'flights/new'
+  get 'flights/create'
+  get 'flights/edit'
+  get 'flights/update'
+  get 'flights/destroy'
   devise_for :users
+
+  root to: 'flights#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
