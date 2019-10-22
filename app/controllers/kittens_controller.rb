5481de83193bc63c64236cac3a76e19d5a5c78ea
TheWillGabriel <will.gabriel@gmail.com>
2019-10-22 13:28:10 -0600

    Build kittens actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,48 @@
+# frozen_string_literal: true
+
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.save
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
   end
+
+  private
+
+    def kitten_params
+      require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
