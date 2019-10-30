de3f742d7115e101c2df447c8f5a37683b88a5bb
TheWillGabriel <will.gabriel@gmail.com>
2019-10-30 15:28:45 -0600

    Handle case of missing website e.g. vacant house seat

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zip(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -10,14 +11,11 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
+    ).officials
   rescue
     'Find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -26,15 +24,15 @@ end
 contents = CSV.open 'event_attendees.csv', headers: true,
                                            header_converters: :symbol
 
-template_letter = File.read 'form_letter.html'
+template_letter = File.read 'form_letter.erb'
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zip(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  puts form_letter
 end
