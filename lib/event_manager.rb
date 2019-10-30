d3c04c4baf24ab0c997f11697fdc8b8966b48483
TheWillGabriel <will.gabriel@gmail.com>
2019-10-30 14:11:47 -0600

    Clean up zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,5 +6,10 @@ contents = CSV.open 'event_attendees.csv', headers: true,
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  zipcode = '00000' if zipcode.nil?
+  zipcode = zipcode[0..4] if zipcode.length > 5
+  zipcode = zipcode.rjust(5, '0') if zipcode.length < 5
+  
   puts "#{name} #{zipcode}"
 end
