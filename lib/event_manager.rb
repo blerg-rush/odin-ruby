53330688e7d3febe735b728b96a19535ca01926c
TheWillGabriel <will.gabriel@gmail.com>
2019-10-30 14:40:21 -0600

    Add each person's legislators from Google Civic API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,26 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zip(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(', ')
+  rescue
+    'Find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts 'EventManager Initialized!'
@@ -10,7 +29,8 @@ contents = CSV.open 'event_attendees.csv', headers: true,
                                            header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  zipcode = clean_zip(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{clean_zip(zipcode)}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
