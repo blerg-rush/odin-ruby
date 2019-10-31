0fb49731ab28bc926b7c8465f1b1c5f72e2028d6
TheWillGabriel <will.gabriel@gmail.com>
2019-10-31 08:01:33 -0600

    Define necessary helper methods for guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,12 +1,35 @@
-require "pry"
+# frozen_string_literal: true
+
+require 'pry'
 
 class Game
-  WORDS = File.open("5desk.txt", "r", &:read).split("\r\n")
+  WORDS = File.open('5desk.txt', 'r', &:read).split("\r\n")
               .select { |word| word.length.between?(5, 12) }
 
-  def initialize
-    @word = WORDS.sample
+  def initialize(guesses = 6)
+    @word = WORDS.sample.downcase
+    @letters_guessed = []
+    @guesses = guesses
+    @hint = @word.gsub(/./, '_').split('')
   end
+
+  def guess(letter)
+    # downcase letter
+    # Return false if letter already guessed
+
+    # Add letter to letters guessed
+
+    # Check letter against word
+    #  If correct, update hint
+
+    # Return hint
+  end
+
+  def update_hint(letter); end
+
+  def misses; end
+
+  def game_over; end
 end
 
 game = Game.new
