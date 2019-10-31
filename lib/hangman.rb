84edeef8f1badd10010fa8061b8b674d1c1f6c6e
TheWillGabriel <will.gabriel@gmail.com>
2019-10-31 07:36:00 -0600

    Assign only words between 5 and 12 characters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,4 +2,12 @@ require "pry"
 
 class Game
   WORDS = File.open("5desk.txt", "r", &:read).split("\r\n")
+              .select { |word| word.length.between?(5, 12) }
+
+  def initialize
+    @word = WORDS.sample
+  end
 end
+
+game = Game.new
+binding.pry
