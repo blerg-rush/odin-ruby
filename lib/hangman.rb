66dc936dcf154aa591e449e814b8562e0569a962
TheWillGabriel <will.gabriel@gmail.com>
2019-10-31 09:27:25 -0600

    Add noose and parts to Display class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -41,19 +41,52 @@ class Game
 end
 
 class Display
-  HEAD = 'O'
-  TORSO = '|'
-  LEFT_LIMB = '/'
-  RIGHT_LIMB = '\\'
+  attr_reader :noose
+  HEAD_PART = 'O'
+  TORSO_PART = '|'
+  LEFT_LIMB_PART = '/'
+  RIGHT_LIMB_PART = '\\'
 
-  def initialize
+  # zero-indexed
+  HEAD = { row: 2, col: 2 }.freeze
+  TORSO = { row: 3, col: 2 }.freeze
+  LEFT_ARM = { row: 3, col: 1 }.freeze
+  RIGHT_ARM = { row: 3, col: 3 }.freeze
+  LEFT_LEG = { row: 4, col: 1 }.freeze
+  RIGHT_LEG = { row: 4, col: 3 }.freeze
+
+  def initialize(failures = 0)
     @noose = File.open('noose.txt', 'r', &:read).split("\n")
-    @failures = 0
+    @width = 24
+    @failures = failures
   end
+
+  def add_part
+    @failures += 1
+    case @failures
+    when 1
+      @noose[HEAD[:row]][HEAD[:col]] = HEAD_PART
+    when 2
+      @noose[TORSO[:row]][TORSO[:col]] = TORSO_PART
+    when 3
+      @noose[LEFT_ARM[:row]][LEFT_ARM[:col]] = LEFT_LIMB_PART
+    when 4
+      @noose[RIGHT_ARM[:row]][RIGHT_ARM[:col]] = RIGHT_LIMB_PART
+    when 5
+      @noose[LEFT_LEG[:row]][LEFT_LEG[:col]] = LEFT_LIMB_PART
+    when 6
+      @noose[RIGHT_LEG[:row]][RIGHT_LEG[:col]] = RIGHT_LIMB_PART
+    end
+  end
+
+  def hint; end
 end
 
 class Hangman
   @display = Display.new
   @game = Game.new
+
+  binding.pry
 end
 
+hangman = Hangman.new
