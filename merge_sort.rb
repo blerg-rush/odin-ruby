687927035d89a4e8dbda90b9723cfe7cd65d9340
TheWillGabriel <will.gabriel@gmail.com>
2019-11-03 11:00:27 -0700

    Finish merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,17 +1,30 @@
-def merge_sort(array)
+# frozen_string_literal: true
 
+require 'pry'
+
+def merge_sort(array)
+  return array if array.size < 2
+
+  half = array.size / 2
+  a = merge_sort(array[0, half])
+  b = merge_sort(array[half, array.size - half])
+  merge(a, b)
 end
 
 def merge(one, two)
   merged_array = []
   until one.empty? && two.empty?
-    if one.empty?
-      merged_array << two.shift
-    elsif two.empty? || one.first < two.first
-      merged_array << one.shift
-    else
-      merge_array << two.shift
-    end
+    merged_array << if one.empty?
+                      two.shift
+                    elsif two.empty? || one.first < two.first
+                      one.shift
+                    else
+                      two.shift
+                    end
   end
   merged_array
 end
+
+p merge_sort [1, 6, 3, 12, 5, 300, 12, 2, 6, 8, 103]
+
+# Split the array (until size < 2)
