6acb75ddaaf267dccbb857baf1a35db802891f19
TheWillGabriel <will.gabriel@gmail.com>
2019-11-03 16:58:10 -0700

    Create append method

--- linked-list.rb
+++ linked-list.rb
@@ -1,8 +1,19 @@
+require 'pry'
+
 class LinkedList
   attr_accessor :head
 
   def initialize
     @head = nil
+  end
+
+  def append(node)
+    return @head = node if @head.nil?
+
+    current_node = @head
+    current_node = current_node.next_node until current_node.next_node.nil?
+
+    current_node.next_node = node
   end
 end
 
@@ -14,3 +25,16 @@ class Node
     @next_node = nil
   end
 end
+
+# list = LinkedList.new
+# node1 = Node.new
+# node1.value = 1
+# list.append node1
+# node2 = Node.new
+# node2.value = 2
+# list.append node2
+# node3 = Node.new
+# node3.value = 3
+# list.append node3
+# p list.head
+# p node1.next_node
