6b3f44f524f46a62528423dcefa3558845942765
TheWillGabriel <will.gabriel@gmail.com>
2019-11-04 09:45:16 -0700

    Create build_branch helper method

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -4,6 +4,21 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def build_tree(array)
+  end
+
+  private
+
+    def build_branch(array)
+      node_index = array.size / 2
+      node = new Node(array[node_index])
+      return node if node_index.zero?
+
+      node.left_child = build_branch(array[0..node_index - 1])
+      node.right_child = build_branch(array[node_index + 1..-1])
+      node
+    end
 end
 
 class Node
