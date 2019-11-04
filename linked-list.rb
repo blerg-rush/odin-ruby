9a4edce322add3734f589ee4a57fd44b7b8f919b
TheWillGabriel <will.gabriel@gmail.com>
2019-11-03 19:05:29 -0700

    Create contains? method

--- linked-list.rb
+++ linked-list.rb
@@ -66,6 +66,16 @@ class LinkedList
     previous_node.next_node = nil
     current_node
   end
+
+  def contains?(value)
+    current_node = @head
+    until current_node.next_node.nil?
+      return true if current_node.value == value
+
+      current_node = current_node.next_node
+    end
+    current_node.value == value
+  end
 end
 
 class Node
@@ -77,42 +87,20 @@ class Node
   end
 end
 
-list = LinkedList.new
+# list = LinkedList.new
 
-node1 = Node.new
-node1.value = 1
-list.prepend node1
-p list.pop
-puts list.size
-p node1
+# node1 = Node.new
+# node1.value = 1
+# list.prepend node1
 
-list.prepend node1
-puts list.size
-node2 = Node.new
-node2.value = 2
-list.append node2
-puts list.size
-node3 = Node.new
-node3.value = 3
-list.append node3
-puts list.size
-p list.head
-p list.head.next_node
-node4 = Node.new
-node4.value = 4
-list.prepend node4
-puts list.size
-p list.head
-p list.head.next_node
-puts list.tail.value
-p list.at(3)
-p list.at(0)
-p list.at(4)
-p list.pop
-puts list.size
-p list.pop
-puts list.size
-p list.pop
-puts list.size
-p list.tail
-p list.pop
+# node2 = Node.new
+# node2.value = 2
+# list.append node2
+
+# node3 = Node.new
+# node3.value = 3
+# list.append node3
+
+# node4 = Node.new
+# node4.value = 4
+# list.prepend node4
