03ad0245dab07b9644d77ee9f5bf67714acd39a5
TheWillGabriel <will.gabriel@gmail.com>
2019-11-04 08:26:47 -0700

    Create Node class

---
+++ binary-search-tree.rb
@@ -0,0 +1,13 @@
+class Tree
+
+end
+
+class Node
+  attr_accessor :data, :left_child, :right_child
+
+  def initialize(data = nil)
+    @data = data
+    @left_child = nil
+    @right_child = nil
+  end
+end
