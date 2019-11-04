fa2dd20f63464d9c24bfbf90796649ab44daf6b5
TheWillGabriel <will.gabriel@gmail.com>
2019-11-04 08:29:23 -0700

    Add Comparable mixin to Node class

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -3,11 +3,16 @@ class Tree
 end
 
 class Node
+  include Comparable
   attr_accessor :data, :left_child, :right_child
 
   def initialize(data = nil)
     @data = data
     @left_child = nil
     @right_child = nil
+  end
+
+  def <=>(other)
+    data <=> other.data
   end
 end
