87541ef289febbd4a43eab857f1eb62e05d41fdd
TheWillGabriel <will.gabriel@gmail.com>
2019-11-04 10:02:41 -0700

    add build_tree method

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -6,13 +6,15 @@ class Tree
   end
 
   def build_tree(array)
+    sorted_array = array.sort.uniq
+    @root = build_branch(sorted_array)
   end
 
   private
 
     def build_branch(array)
       node_index = array.size / 2
-      node = new Node(array[node_index])
+      node = Node.new(array[node_index])
       return node if node_index.zero?
 
       node.left_child = build_branch(array[0..node_index - 1])
@@ -35,3 +37,7 @@ class Node
     data <=> other.data
   end
 end
+
+tree = Tree.new
+tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p tree.root
