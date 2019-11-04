c1098d694c128e6d36cce02675b7c00eb5e87039
TheWillGabriel <will.gabriel@gmail.com>
2019-11-03 17:17:08 -0700

    Refactor append to use tail method

--- linked-list.rb
+++ linked-list.rb
@@ -10,10 +10,7 @@ class LinkedList
   def append(node)
     return @head = node if @head.nil?
 
-    current_node = @head
-    current_node = current_node.next_node until current_node.next_node.nil?
-
-    current_node.next_node = node
+    tail.next_node = node
   end
 
   def prepend(node)
@@ -52,25 +49,25 @@ class Node
   end
 end
 
-# list = LinkedList.new
-# node1 = Node.new
-# node1.value = 1
-# list.prepend node1
-# puts list.size
-# node2 = Node.new
-# node2.value = 2
-# list.append node2
-# puts list.size
-# node3 = Node.new
-# node3.value = 3
-# list.append node3
-# puts list.size
-# p list.head
-# p list.head.next_node
-# node4 = Node.new
-# node4.value = 4
-# list.prepend node4
-# puts list.size
-# p list.head
-# p list.head.next_node
-# puts list.tail.value
+list = LinkedList.new
+node1 = Node.new
+node1.value = 1
+list.prepend node1
+puts list.size
+node2 = Node.new
+node2.value = 2
+list.append node2
+puts list.size
+node3 = Node.new
+node3.value = 3
+list.append node3
+puts list.size
+p list.head
+p list.head.next_node
+node4 = Node.new
+node4.value = 4
+list.prepend node4
+puts list.size
+p list.head
+p list.head.next_node
+puts list.tail.value
