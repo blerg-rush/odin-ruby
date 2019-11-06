9da9350b28a264e68381fc334fedd3ab19d91a7d
TheWillGabriel <will.gabriel@gmail.com>
2019-11-06 08:41:14 -0700

    Initialize Chessboard and Knight classes

---
+++ knight_travels.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Chessboard
+  class Knight
+    attr_accessor :pos
+    attr_reader :moves
+    def initialize(pos = nil)
+      @moves = [[-2, 1], [-1, 2], [1, 2], [2, 1],
+                [-2, -1], [-1, -2], [1, -2], [2 - 1]]
+      @pos = pos
+    end
+  end
+
+  attr_accessor :start, :end
+
+  def initialize(start, finish)
+    @knight = Knight.new
+    @start = start
+    @finish = finish
+  end
+end
