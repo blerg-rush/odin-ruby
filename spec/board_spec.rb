9a58c0c2ab8b6c377e8b5b99f82ba582a7d931a5
TheWillGabriel <will.gabriel@gmail.com>
2019-11-08 08:35:45 -0700

    Run tests on instance of Board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,35 +1,37 @@
+require './lib/board'
+
 RSpec.describe Board do
   before(:each) do
-    @grid = Grid.new
+    @board = Board.new
   end
 
-  describe '#add(row, player)' do
+  describe '#add(column, player)' do
     it 'adds a piece to row 0 of an empty column' do
-      add(3, 1)
-      expect(@grid[0][3]).to eql(1)
+      @board.add(3, 1)
+      expect(@board.grid[0][3]).to eql(1)
     end
 
     it 'adds a piece to the next empty row of a column' do
-      add(0, 2)
-      add(0, 2)
-      expect(@grid[1][0]).to eql(2)
+      @board.add(0, 2)
+      @board.add(0, 2)
+      expect(@board.grid[1][0]).to eql(2)
     end
 
     it 'returns the coordinates of the piece placed' do
-      expect(add(6, 1)).to eql([0, 6])
+      expect(@board.add(6, 1)).to eql([0, 6])
     end
 
     it 'returns nil if the column index is greater than 6' do
-      expect(add(7, 2)).to eql(nil)
+      expect(@board.add(7, 2)).to eql(nil)
     end
 
     it 'returns nil if the column index is less than 0' do
-      expect(add(-1, 1)).to eql(nil)
+      expect(@board.add(-1, 1)).to eql(nil)
     end
 
     it 'returns nil if the column is full' do
-      6.times { add(0, 1) }
-      expect(add(0, 1)).to eql(nil)
+      6.times { @board.add(0, 1) }
+      expect(@board.add(0, 1)).to eql(nil)
     end
   end
 end
