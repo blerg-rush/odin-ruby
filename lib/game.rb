ad01e10e3e9be93e9487173261c696f7f6a374d7
TheWillGabriel <will.gabriel@gmail.com>
2019-11-07 17:01:07 -0700

    Define specs for Board#add

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ require_relative 'board'
 # Stores game state and flow
 class Game
   attr_reader :board, :player1, :player2
+
   def initialize(player1, player2)
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
---
+++ spec/board_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe Board do
+  before(:each) do
+    @grid = Grid.new
+  end
+
+  describe '#add' do
+    it 'adds a piece to row 0 of an empty column' do
+    end
+
+    it 'adds a piece to the next empty row of a column' do
+    end
+
+    it 'returns the coordinates of the piece placed' do
+    end
+
+    it 'returns nil if the column is invalid' do
+    end
+
+    it 'returns nil if the column is full' do
+    end
+  end
+end
