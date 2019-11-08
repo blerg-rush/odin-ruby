07c1869b75402888a1b76d529f7aebafbfbc991e
TheWillGabriel <will.gabriel@gmail.com>
2019-11-08 07:28:50 -0700

    Create Board#add method

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,16 @@ class Board
     # Columns nested inside rows
     @grid = Array.new(6) { Array.new(7) }
   end
+
+  def add(row, player)
+    return nil unless row.between?(0, 7)
+
+    @grid[row].each do |column|
+      if column.nil?
+        @grid[row][column] = player
+        return [row, column]
+      end
+    end
+    nil
+  end
 end
