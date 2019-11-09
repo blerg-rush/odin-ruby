95af360b8def68d4e790f2a4596cd0c2660ca7c8
TheWillGabriel <will.gabriel@gmail.com>
2019-11-09 14:34:46 -0700

    Add tests for #pick_first_player

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe Game do
   end
 
   describe '#pick_first_player' do
-    it 'assigns player1 or player2 to current_player' do
+    it 'assigns one of player1 or player2 to current_player' do
+      @game.pick_first_player
+      expect([@game.player1, @game.player2]).to include(@game.first_player)
     end
 
-    it 'returns nil if randomization is skipped' do
-    end
-
-    it 'skips randomization if @game initialized with first_player' do
+    it 'returns a Player object' do
+      @game = Game.new('Foo', 'Bar', 'Foo')
+      expect(@game.pick_first_player).to be_instance_of Player
     end
   end
 end
