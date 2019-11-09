414abe338bcc2fd065c6066cea3579c3a6ffb0d8
TheWillGabriel <will.gabriel@gmail.com>
2019-11-09 14:44:02 -0700

    Add pick_first_player method

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,19 @@ require_relative 'board'
 
 # Stores game state and flow
 class Game
-  attr_reader :board, :player1, :player2
+  attr_reader :board, :player1, :player2, :current_player
 
-  def initialize(player1, player2)
+  def initialize(player1, player2, first_player = nil)
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
+    @current_player = pick_first_player(first_player)
     @board = Board.new
   end
+
+  def pick_first_player(first_player)
+    return [@player1, @player2].sample if first_player.nil?
+
+    [@player1, @player2].select { |player| player.name == first_player }
+  end
+
 end
