0cad79b78693a8f0c5635726bc701eaa71a822bb
TheWillGabriel <will.gabriel@gmail.com>
2019-11-10 13:23:56 -0700

    Move constants outside of class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,21 +1,21 @@
 require_relative 'game'
 require 'colorize'
 
+HORIZ = "\u2550\u2550".encode('utf-8')
+VERTI = "\u2551".encode('utf-8')
+TOPLT = "\u2554".encode('utf-8')
+TOPMD = "\u2566".encode('utf-8')
+TOPRT = "\u2557".encode('utf-8')
+MIDLT = "\u2560".encode('utf-8')
+MIDMD = "\u256C".encode('utf-8')
+MIDRT = "\u2563".encode('utf-8')
+BOTLT = "\u255A".encode('utf-8')
+BOTMD = "\u2569".encode('utf-8')
+BOTRT = "\u255D".encode('utf-8')
+
 # Presents game to users
 class ConnectFour
   attr_reader :game
-
-  HORIZ = "\u2550\u2550".encode('utf-8')
-  VERTI = "\u2551".encode('utf-8')
-  TOPLT = "\u2554".encode('utf-8')
-  TOPMD = "\u2566".encode('utf-8')
-  TOPRT = "\u2557".encode('utf-8')
-  MIDLT = "\u2560".encode('utf-8')
-  MIDMD = "\u256C".encode('utf-8')
-  MIDRT = "\u2563".encode('utf-8')
-  BOTLT = "\u255A".encode('utf-8')
-  BOTMD = "\u2569".encode('utf-8')
-  BOTRT = "\u255D".encode('utf-8')
 
   def initialize(player1, player2)
     @game = Game.new(player1, player2)
