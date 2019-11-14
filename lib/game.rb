62d43f69a48dfabda129237bb362d218cc374ed4
TheWillGabriel <will.gabriel@gmail.com>
2019-11-14 15:27:43 -0700

    Add method to convert chess notation to indices

--- lib/game.rb
+++ lib/game.rb
@@ -17,6 +17,17 @@ class Game
   private
 
 
+    def convert_to_index(file_rank)
+      unless ('A'..'H').include?(file_rank[0]) && (1..8).include?(file_rank[1])
+        return nil
+      end
+
+      col = %w[A B C D E F G H].index(file_rank[0].upcase)
+      row = file_rank[1] - 1
+
+      [row, col]
+    end
+
     def render_display
       @display.render(@message)
     end
